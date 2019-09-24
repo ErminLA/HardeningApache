07f0e3c9859f03f11ce8379060597f310cb6d728
ErminLA <42754356+ErminLA@users.noreply.github.com>
2019-09-24 23:31:15 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+wget https://github.com/ErminLA/HardeningApache/archive/master.zip
+yum install unzip -y
+unzip master.zip
+mkdir /home/cloud_user/Backend
+mv HardeningApache-master/app.py /home/cloud_user/Backend/
+chown -Rv cloud_user:cloud_user /home/cloud_user/Backend/
+mv HardeningApache-master/flaskapp.service /etc/systemd/system/
+yum install epel-release -y
+yum install python36.x86_64 -y
+yum install python36-devel.x86_64 -y
+yum groupinstall "Development Tools" -y
+yum install python36-pip -y
+ln -s /usr/local/bin/pip3 /usr/bin/pip3
+pip3 install Flask
+pip3 install flask-cors
+pip3 install psutil
+yum install tcpdump -y
+
+firewall-cmd --permanent --add-port=65535/tcp
+firewall-cmd --reload
+
+systemctl daemon-reload
+systemctl start flaskapp.service
+systemctl enable flaskapp
