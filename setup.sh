ec953e6e3832ca93917b246746b05efbe74f4d1c
ErminLA <42754356+ErminLA@users.noreply.github.com>
2019-09-25 01:16:07 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -27,6 +27,36 @@ firewall-cmd --permanent --add-port=22/tcp
 firewall-cmd --reload
 systemctl enable firewalld
 
+yum install httpd -y
+systemctl start httpd
+systemctl enable httpd
+
 systemctl daemon-reload
 systemctl start flaskapp.service
 systemctl enable flaskapp
+
+hostname="$(cat /etc/hostname)"
+mkdir -p /var/www/$hostname/html
+mkdir -p /var/www/$hostname/log
+
+chown cloud_user:cloud_user /var/www/$hostname/html/
+chown cloud_user:cloud_user /var/www/$hostname/
+chown cloud_user:cloud_user /var/www/$hostname/log/
+
+mv HardeningApache-master/index.html /var/www/$hostname/html/
+sed -i \"s/$hostname/$MyStr/g\" /var/www/$hostname/html/index.html
+
+mkdir /etc/httpd/sites-available
+mkdir /etc/httpd/sites-enabled
+echo "IncludeOptional sites-enabled/*.conf" >> /etc/httpd/conf/httpd.conf
+
+echo "<VirtualHost *:80>
+	ServerName www.$hostname
+	ServerAlias $hostname
+	DocumentRoot /var/www/$hostname/html
+	ErrorLog /var/www/$hostname/log/error.log
+	CustomLog /var/www/$hostname/log/requests.log 	combined
+</VirtualHost>" > /etc/httpd/sites-available/ermin1c.mylabserver.com.conf
+
+ln -s /etc/httpd/sites-available/ermin1c.mylabserver.com.conf /etc/httpd/sites-enabled/ermin1c.mylabserver.com.conf
+
